7ef5df113116e5444dbed7ef5f60f9755a61673d
andre <andrecabralpires@gmail.com>
2020-09-29 21:15:24 +0100

    params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,10 @@ class PagesController < ApplicationController
   end
 
   def about
-    @company_name = "LeWagon"
+    if params[:my_params] == params[:my_params].upcase
+      @company_name = "CAPS LOCK ON"
+    else
+      @company_name = "CAPS LOCK off"
+    end
   end
 end
